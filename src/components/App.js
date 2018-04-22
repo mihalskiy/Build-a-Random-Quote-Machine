0b9bff70f32930f9a461e36ad59cc33e0ffeb2b0
mihalskiy <mihalskiy.rv@gmail.com>
2018-04-23 00:14:56 +0300

    added some texture

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import Article from './Article';
+//import Article from './Article';
+import ArticleList from './ArticleList';
 import articles from '../fixtures';
 
 function App () {
+
     return (
         <div>
             <h1>app name</h1>
-            <Article article={articles[0]} />
+            {/*<Article article={articles[0]} flag foo="bar"/>*/}
+            <ArticleList articles={articles}/>
         </div>
     )
 }
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Article (props) {
-    const {article} = props;
-    const body = <section>{article.title}</section>;
-    //const date = new Date().toDateString();
-    return (
-        <div>
-            <h1>{article.title}</h1>
-            {body}
-            <h3>creation date: {article.date}</h3>
-        </div>
-    )
+class Article extends Component{
+
+/*    constructor(props) {
+        super(props)
+
+        this.state = {
+            isOpen: true
+        };
+
+        this.hendleClose = hendleClose.bind(this);
+    }*/
+
+    state = {
+        isOpen: true
+    };
+
+
+    render() {
+
+        const {article} = this.props;
+        const body = this.state.isOpen && <section>{article.text}</section>;
+        //const date = new Date().toDateString();
+        console.log(this.props);
+        return (
+            <div>
+                <h1>{article.title}</h1>
+                <button onClick={this.hendleClose}>{this.state.isOpen ? 'Close':'Open'}</button>
+                {body}
+                <h3>creation date: {article.date}</h3>
+            </div>
+        )
+    }
+
+    hendleClose =  () => {
+        console.log('_____');
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
 }
 
 export default Article;
