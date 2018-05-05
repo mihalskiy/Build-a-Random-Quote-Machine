a9caf910b0f56bf52d79f3351215edd6363ae1f8
mihalskiy <mihalskiy.rv@gmail.com>
2018-05-05 22:54:58 +0300

    added random colors

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './article.css';
 
-//import Article from './Article';
-
 class ArticleList extends Component {
     constructor(props) {
         super(props);
@@ -37,25 +35,42 @@ class ArticleList extends Component {
             )
     }
 
-    onSort () {
-        console.log('sorted')
-        //state.items.sort();
+    onReload () {
+        window.location.reload()
     }
 
     render() {
         const { error, isLoaded, random } = this.state;
-        //const rand = Math.floor(Math.random() * items.length);
+        const colors = [
+            '#16a085',
+            '#27ae60',
+            '#2c3e50',
+            '#f39c12',
+            '#e74c3c',
+            '#9b59b6',
+            '#FB6964',
+            '#342224',
+            "#472E32",
+            "#BDBB99",
+            "#77B1A9",
+            "#73A857"
+        ];
+        const rand = Math.floor(Math.random() * colors.length);
+        const randomColor = {
+            background: colors[rand],
+        };
+        console.log(colors[rand]);
         if (error) {
             return <div>Error: {error.message}</div>;
         } else if (!isLoaded) {
             return <div>Loading...</div>;
         } else {
             return (
-                <div className="article">
-                        <section className="articleList">
+                <div className="article" style={randomColor}>
+                        <section className="articleList" >
                             <p className="articleTitle">{random.elementPureHtml}</p>
                             <a href={random.site} target="_blank"><span className="articleAuthor">{random.desc}</span></a>
-                            <button onClick={this.onSort}>Новая цитата</button>
+                            <button onClick={this.onReload}>Новая цитата</button>
                         </section>
                 </div>
             );
